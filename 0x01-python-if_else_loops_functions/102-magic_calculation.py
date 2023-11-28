7d0ba2d2cfa1561333723cd754bc0bdf621926ed
gandghub <hackthehacker33@gmail.com>
2023-11-28 07:03:55 -0800

    0x01-python-if_else_loops_functions

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 # Author - Gemechis
-
 def magic_calculation(a, b, c):
-       if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return a * b - c
